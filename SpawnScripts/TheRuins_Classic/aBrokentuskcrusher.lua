5403a86db80f048115ce8d40e6136409dc934ec1
Cynnar <jacob0305@gmail.com>
2023-08-05 10:42:09 +0000

    Updated 05/08/2023

---
+++ SpawnScripts/TheRuins_Classic/aBrokentuskcrusher.lua
@@ -0,0 +1,67 @@
+--[[
+    Script Name    : SpawnScripts/TheRuins_Classic/aBrokentuskcrusher.lua
+    Script Author  : LordPazuzu
+    Script Date    : 2023.08.05 05:08:34
+    Script Purpose : 
+                   : 
+--]]
+
+dofile("SpawnScripts/Generic/MonsterCallouts/BrokenTusk1.lua")
+
+function spawn(NPC)
+    local Level = GetLevel(NPC)
+    local level1 = 9
+    local level2 = 10
+    local level3 = 11
+    local difficulty1 = 6
+    local hp1 = 275
+    local power1 = 110
+    local difficulty2 = 6
+    local hp2 = 370
+    local power2 = 130
+    local difficulty3 = 6
+    local hp3 = 430
+    local power3 = 160
+    if Level == level1 then
+    SpawnSet(NPC, "difficulty", difficulty1)
+    SpawnSet(NPC, "hp", hp1)
+    SpawnSet(NPC, "power", power1)
+    elseif Level == level2
+        then
+    SpawnSet(NPC, "difficulty", difficulty2)
+    SpawnSet(NPC, "hp", hp2)
+    SpawnSet(NPC, "power", power2)
+    elseif Level == level3
+        then
+    SpawnSet(NPC, "difficulty", difficulty3)
+    SpawnSet(NPC, "hp", hp3)
+    SpawnSet(NPC, "power", power3)
+    end
+    
+    
+ AddTimer(NPC,MakeRandomInt(10000,15000),"EmoteLoop")
+end
+
+function respawn(NPC)
+    spawn(NPC)
+end
+
+function EmoteLoop(NPC,Spawn)
+   if IsInCombat(NPC) == false then
+        choice = MakeRandomInt(1,5)
+        if choice == 1 then
+            PlayAnimation(NPC, 717)
+        elseif choice == 2 then
+            PlayAnimation(NPC, 717)
+        elseif choice == 3 then
+            PlayAnimation(NPC, 891)
+        elseif choice == 4 then
+            PlayAnimation(NPC, 713)
+        elseif choice == 5 then
+            PlayAnimation(NPC, 553)
+end
+    AddTimer(NPC,MakeRandomInt(10000,15000),"EmoteLoop")    
+else
+    AddTimer(NPC,MakeRandomInt(10000,15000),"EmoteLoop") 
+end
+end
---
+++ SpawnScripts/TheRuins_Classic/aBrokentuskprophet.lua
@@ -0,0 +1,67 @@
+--[[
+    Script Name    : SpawnScripts/TheRuins_Classic/aBrokentuskprophet.lua
+    Script Author  : LordPazuzu
+    Script Date    : 2023.08.05 06:08:45
+    Script Purpose : 
+                   : 
+--]]
+
+dofile("SpawnScripts/Generic/MonsterCallouts/BrokenTusk1.lua")
+
+function spawn(NPC)
+    local Level = GetLevel(NPC)
+    local level1 = 9
+    local level2 = 10
+    local level3 = 11
+    local difficulty1 = 6
+    local hp1 = 275
+    local power1 = 110
+    local difficulty2 = 6
+    local hp2 = 370
+    local power2 = 130
+    local difficulty3 = 6
+    local hp3 = 430
+    local power3 = 160
+    if Level == level1 then
+    SpawnSet(NPC, "difficulty", difficulty1)
+    SpawnSet(NPC, "hp", hp1)
+    SpawnSet(NPC, "power", power1)
+    elseif Level == level2
+        then
+    SpawnSet(NPC, "difficulty", difficulty2)
+    SpawnSet(NPC, "hp", hp2)
+    SpawnSet(NPC, "power", power2)
+    elseif Level == level3
+        then
+    SpawnSet(NPC, "difficulty", difficulty3)
+    SpawnSet(NPC, "hp", hp3)
+    SpawnSet(NPC, "power", power3)
+    end
+AddTimer(NPC,MakeRandomInt(15000,20000),"EmoteLoop")
+end
+
+function respawn(NPC)
+    spawn(NPC)
+end
+
+function EmoteLoop(NPC,Spawn)
+   if IsInCombat(NPC) == false then
+        choice = MakeRandomInt(1,5)
+        if choice == 1 then
+            CastSpell(NPC, 110002, 5, NPC)
+        elseif choice == 2 then
+            CastSpell(NPC, 110003, 5, NPC)
+            PlayFlavor(NPC,"","","yawn",0,0)
+--          PlayAnimation(NPC, 891)
+        elseif choice == 4 then
+            PlayFlavor(NPC,"","","tapfoot",0,0)
+--          PlayAnimation(NPC, 713)
+        elseif choice == 5 then
+            PlayFlavor(NPC,"","","sniff",0,0)
+--          PlayAnimation(NPC, 553)
+end
+    AddTimer(NPC,MakeRandomInt(15000,20000),"EmoteLoop")    
+else
+    AddTimer(NPC,MakeRandomInt(15000,20000),"EmoteLoop") 
+end
+end
